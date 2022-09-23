c4992129b05dfc2c2a87209489a85550bd3bd9e1
shubham <shubhambkhatal10@gmail.com>
2022-09-23 19:04:01 +0530

    curd

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from '@firebase/firestore' ;
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCjnhCnb_iSyXD0BCWnr5BGOIN1wmhW2QA",
-    authDomain: "curd-8bba8.firebaseapp.com",
-    projectId: "curd-8bba8",
-    storageBucket: "curd-8bba8.appspot.com",
-    messagingSenderId: "874591369998",
-    appId: "1:874591369998:web:6e3dfdf357834659ceb55f"
-};
-
+    apiKey: "AIzaSyCGNJWKri51_BSd0uoy6CA-pbGrveUH-fk",
+    authDomain: "curd-c2eb8.firebaseapp.com",
+    projectId: "curd-c2eb8",
+    storageBucket: "curd-c2eb8.appspot.com",
+    messagingSenderId: "373208188876",
+    appId: "1:373208188876:web:6c5db8cc5f1debf1f53f69"
+  };
+  
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
